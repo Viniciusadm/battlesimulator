6bee7369a91bc50aa917347d0cb17b5addf374af
viniciusam <viniciusam.22@gmail.com>
2023-04-17 22:14:26 -0300

    Tela pra mostrar a batalha

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,69 @@
+import { Player } from "@/App/Player";
+import { Battle } from "@/App/Battle";
+import { useState } from "react";
+import { d100, d20 } from "@/functions/dices";
+
 export default function Home() {
-	return (
-		<main className="flex flex-col items-center py-2 max-w-2xl mx-auto">
-			<h1 className="text-4xl font-bold">Battle Simulator</h1>
-		</main>
-	);
-  }
-  
+    const [logsInScreen, setLogsInScreen] = useState<string[]>([]);
+
+    const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+
+    const startBattle = async () => {
+        setLogsInScreen([]);
+
+        const Kazuma = new Player({
+            name: "Kazuma",
+            life: d100.roll(10, 50),
+        }, {
+            strength: d20.roll(10),
+            dexterity: d20.roll(-5),
+            charisma: d20.roll(-10),
+            constitution: d20.roll(5),
+            intelligence: d20.roll(),
+            wisdom: d20.roll(),
+        });
+
+        const Megumin = new Player({
+            name: "Megumin",
+            life: d100.roll(0, 50),
+        }, {
+            strength: d20.roll(),
+            dexterity: d20.roll(10),
+            charisma: d20.roll(10),
+            constitution: d20.roll(),
+            intelligence: d20.roll(),
+            wisdom: d20.roll(),
+        });
+
+        const battle = new Battle(Kazuma, Megumin);
+        battle.fight();
+        const logs = battle.getLogs();
+
+        for (const log of logs) {
+            setLogsInScreen((prev) => [...prev, log]);
+            await sleep(1000);
+        }
+    }
+
+    return (
+        <main className="flex flex-col items-center py-2 mx-auto">
+            <h1 className="text-4xl font-bold">Battle Simulator</h1>
+
+            <button onClick={startBattle} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4">
+                Start Battle
+            </button>
+
+            <div className="flex flex-col items-center">
+                {logsInScreen.map((log, index) => (
+                    <p
+                        key={index}
+                        className="mb-2"
+                    >
+                        {log}
+                    </p>
+                ))}
+            </div>
+
+        </main>
+    );
+}
