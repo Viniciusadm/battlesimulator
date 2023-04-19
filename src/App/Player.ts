0a0338fc991796025c86aa8b7a764b6e566d2615
viniciusam <viniciusam.22@gmail.com>
2023-04-19 01:57:08 -0300

    Organização da classe Player

--- src/App/Player.ts
+++ src/App/Player.ts
@@ -1,11 +1,5 @@
 import { d6, d20 } from "@/functions/dices";
-import Inventory from "@/App/Inventory";
-
-export type PlayerAttributes = {
-    name: string;
-    life: number;
-    totalLife: number;
-};
+import Character from "@/App/Character";
 
 export type PlayerSkills = {
     strength: number;
@@ -16,33 +10,15 @@ export type PlayerSkills = {
     charisma: number;
 };
 
-export class Player extends Inventory {
+export class Player extends Character {
     private readonly skills: PlayerSkills;
-    private attributes: PlayerAttributes;
     private readonly armor: number = 0;
 
     constructor(attributes: { name: string }, skills: PlayerSkills) {
-        super();
+        super(attributes);
         this.skills = skills;
         this.attributes = { ...attributes, life: this.getInitialLife(), totalLife: this.getInitialLife() };
         this.armor = this.getCalculatedArmor();
-    }
-
-    public getName(): string {
-        return this.attributes.name;
-    }
-
-    public getLife(): number {
-        if (!this.attributes.totalLife) return 0;
-        return Number(this.attributes.life / this.attributes.totalLife * 100);
-    }
-
-    public isAlive(): boolean {
-        return this.attributes.life > 0;
-    }
-
-    public isDangerous(): boolean {
-        return this.attributes.life < (this.attributes.totalLife / 2);
     }
 
     public getExpecifiedSkill(skill: keyof PlayerSkills): number {
@@ -57,21 +33,6 @@ export class Player extends Inventory {
         skills += `life: ${this.attributes.life}, `;
         skills += `armor: ${this.armor}`;
         return skills;
-    }
-
-    public decreaseLife(amount: number): number {
-        this.attributes.life -= amount;
-        return this.attributes.life;
-    }
-
-    public increaseLife(amount: number): number {
-        this.attributes.life += amount;
-        return this.attributes.life;
-    }
-
-    public setLifeInMax(): number {
-        this.attributes.life = this.attributes.totalLife as number;
-        return this.attributes.life;
     }
 
     public tryToHit(target: Player): boolean {
