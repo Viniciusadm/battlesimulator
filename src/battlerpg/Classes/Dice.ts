a953a816ca6ca749ee7af72dd5b7db16cd0d54fb
viniciusam <viniciusam.22@gmail.com>
2023-04-22 11:50:55 -0300

    Rolagens retornadas no Dice

--- src/battlerpg/Classes/Dice.ts
+++ src/battlerpg/Classes/Dice.ts
@@ -1,9 +1,11 @@
 type Increments = number | number[];
 
-type roll = {
+type rolls = { dice: string, value: number }[];
+
+export type roll = {
     value: number,
-    dice: number,
     increment: number,
+    rolls: rolls,
 }
 
 export default class Dice {
@@ -19,19 +21,34 @@ export default class Dice {
         const result = dice + increment;
         return {
             value: min ? Math.max(result, min) : result,
-            dice,
             increment,
+            rolls: [{
+                dice: Dice.diceToString(this),
+                value: dice,
+            }],
         };
     }
 
     public static rollMultiple(dices: Dice[], increments: Increments = [], min: number|undefined = undefined): roll {
-        const result = dices.map(dice => dice.roll()).reduce((a, b) => a + b.value, 0);
+        const rolls: { dice: string, value: number }[] = [];
+        const result = dices.map(dice => {
+            const roll = dice.roll();
+            rolls.push({
+                dice: Dice.diceToString(dice),
+                value: roll.value,
+            });
+            return roll;
+        }).reduce((a, b) => a + b.value, 0);
         const increment = Array.isArray(increments) ? increments.reduce((a, b) => a + b, 0) : increments;
         const value = result + increment;
         return {
             value: min ? Math.max(value, min) : value,
-            dice: result,
             increment,
+            rolls,
         }
+    }
+
+    private static diceToString(dice: Dice): string {
+        return `d${dice.sides}`;
     }
 }
