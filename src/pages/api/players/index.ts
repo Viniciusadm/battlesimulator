8f85e80a27caa706de4bb5a8254ef6f0482c6878
viniciusam <viniciusam.22@gmail.com>
2023-04-23 19:43:06 -0300

    Player spells na query

--- src/pages/api/players/index.ts
+++ src/pages/api/players/index.ts
@@ -7,6 +7,10 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const data = await prisma.players.findMany();
+    const data = await prisma.players.findMany({
+        include: {
+            player_spells: true,
+        }
+    });
     res.status(200).json(data);
 }
