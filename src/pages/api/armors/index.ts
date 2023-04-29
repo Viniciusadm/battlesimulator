6798385243c96e8ce103a3f44af5fa8a74de7497
viniciusam <viniciusam.22@gmail.com>
2023-04-29 16:48:11 -0300

    Correção na rota de armors

--- src/pages/api/armors/index.ts
+++ src/pages/api/armors/index.ts
@@ -10,7 +10,27 @@ export default async function handler(
     const data = await prisma.items.findMany({
         where: {
             type: 'armor'
-        }
+        },
+        include: {
+            armors: true,
+        },
     });
-    res.status(200).json(data);
+
+    if (data) {
+        const response = data.map(item => {
+            const armor = item.armors.splice(0, 1)[0];
+
+            return {
+                id: item.id,
+                name: item.name,
+                type: item.type,
+                base: armor.base,
+                additional: armor.additional,
+            }
+        });
+
+        res.status(200).json(response);
+    } else {
+        res.status(500).json({ error: 'Unable to fetch armors' });
+    }
 }
